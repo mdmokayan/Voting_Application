e87a3deecb64fd6d09a4e4ddc2ec536b37277200
Nayan <mdmokayan1997@gmail.com>
2025-05-02 22:09:27 +0530

    comment added

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require ('express')
+const express = require('express')
 const app = express()
 const db = require('./db')
 
@@ -8,10 +8,10 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json()) //req.body
 
 app.get('/', (req, res) => {
-    res.send('Welcome to my voting app')
-  })
+  res.send('Welcome to my voting app')
+})
 
-//   const {jwtAuthMiddleware} = require('./jwt') 
+//   const {jwtAuthMiddleware} = require('./jwt')
 
 // Import the router files and use the router
 const userRoute = require('./routes/userRoutes')
@@ -20,10 +20,9 @@ app.use('/user', userRoute) // use the router
 const candidateRoute = require('./routes/candidateRoutes')
 app.use('/candidate', candidateRoute) // use the router
 
-
+// Comment
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, ()=>{
-    console.log('listening on port 3000')
+app.listen(PORT, () => {
+  console.log('listening on port 3000')
 })
-
